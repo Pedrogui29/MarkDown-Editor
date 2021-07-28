b6c55a310546d4096c99bdab42ab2660ada6ef36
Pedro Oliveira <pedro@pdsolucoes.com.br>
2021-07-28 15:45:54 -0300

    Add markdown editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import ReactMarkdown from 'react-markdown';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './index.css';
+export default function App() {
+const [input,setInput] = useState();
+return (
+ <div className= "App">
+     <textarea
+     autoFocus
+     className= "textarea"
+     value={input}
+     onChange= {(e) => setInput(e.target.value)}
+     />
+     <ReactMarkdown children={input} className="markdown"  /> 
+  </div>
+ );
 }
-
-export default App;
+    
